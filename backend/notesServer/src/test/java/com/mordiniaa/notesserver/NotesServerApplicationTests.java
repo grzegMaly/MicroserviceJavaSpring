b017ca1abc3fd48d351abbba1beecd18cc519a15
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2025-12-25 23:49:07 +0100

    NotesServer Setup

---
+++ backend/notesServer/src/test/java/com/mordiniaa/notesserver/NotesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mordiniaa.notesserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

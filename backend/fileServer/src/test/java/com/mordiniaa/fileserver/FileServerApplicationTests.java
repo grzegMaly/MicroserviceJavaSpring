11ca771870e1a6e2d3ba5da76e00e07d7e196784
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2025-12-25 22:58:40 +0100

    FileServer Startup

---
+++ backend/fileServer/src/test/java/com/mordiniaa/fileserver/FileServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mordiniaa.fileserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
